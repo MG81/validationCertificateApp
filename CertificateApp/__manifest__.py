d6ea0049d43a9af0f979e18de550e12ad889b991
Mostafa Gaber <mostafa.gaber.mg81@gmail.com>
2022-07-17 00:09:18 +0200

    Adding Validation Certificate App Files

---
+++ CertificateApp/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    'name': "Certificate App",
+    'version': '1.0',
+    'depends': ['base','sale'],
+    'author': "Mostafa Gaber",
+    'description': """
+    Validation Certificates App Module
+    """,
+    'data': [
+        'views/certificate_groups.xml',
+        'security/ir.model.access.csv',
+        'views/certificate_types_view.xml',
+        'views/traffic_department_view.xml',
+        'views/res_partner_view.xml',
+        'views/sequence.xml',
+        'views/certificate_view.xml',
+        'reports/reports.xml',
+        'reports/certificate_template.xml',
+
+
+
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'AGPL-3',
+    }
---
+++ CertificateApp/models/__init__.py
@@ -0,0 +1,2 @@
+from . import certificate_types ,traffic_departements
+from . import vehicle_brands , certificate , print_certificate_logs
